92c9c842843ee54b8e6d7183a94ce8a5629069e7
Sakshi123-bil <82723338+Sakshi123-bil@users.noreply.github.com>
2023-05-17 11:22:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+import copy
 const players = ["John", "Bob", "Alice", "Poppy"];
 
 const person = {
   name: "John Doe",
   age: 80,
 };
+
+const team = players;
+const teamp1 = copy.copy(players);
+const cap1 = copy.deepcopy(person);
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
